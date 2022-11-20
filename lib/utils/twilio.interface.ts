4ce38adff7d9064044a05304e11b3fcd320dc541
Lazar Karić <16634314+rejvban@users.noreply.github.com>
2022-11-20 02:41:35 +0100

    release/3.2.0 (#44)

--- lib/utils/twilio.interface.ts
+++ lib/utils/twilio.interface.ts
@@ -2,7 +2,10 @@ import { Twilio } from 'twilio';
 
 export type TwilioClient = Twilio;
 
-export interface TwilioModuleOptions {
+export interface ExtraConfiguration {
+  isGlobal?: boolean;
+}
+export interface TwilioModuleOptions extends ExtraConfiguration {
   accountSid: string | undefined;
   authToken: string | undefined;
   options?: Twilio.TwilioClientOptions | undefined;
